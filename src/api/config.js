42f74ecff05d7e4deb513323e53cf217f0b32b2e
aelapierre <lapierre82@gmail.com>
2019-08-06 23:51:17 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+export default {
+    appName: 'my-music',
+    apiKey: '7cc7bf13491d19d5bd1b090bf600efe6',
+    sharedSecret: '127eb4b78befb49d8b9f263261cec303',
+    registeredTo: 'alanlapierre'
+}
+	
