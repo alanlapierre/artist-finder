3c80a7f76f2c5ced0eda169107c00673b6419ef6
aelapierre <lapierre82@gmail.com>
2019-08-11 09:52:29 -0300

    Correccion en estilo de CARDs de artistas

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 v-app.mt-0(dark)
   v-container
-    img(src='./assets/logo.png')
+    // img(src='./assets/logo.png')
     h1.display-3.white--text ArtistFinder
       v-select(:items="countries" v-model="selectedCountry" label="Seleccione el país" single-line)
     h1.display-2.white--text Seleccione un país del listado para ver los artistas TOP.
     
     spinner(v-show="loading")
     v-layout.mt-3(row, wrap)
-      v-flex(v-for="artist in artists" v-bind:key="artist.mbid" xs12, sm6, md2)
+      v-flex(v-for="artist in artists" v-bind:key="artist.mbid" xs12, sm6, md4)
         artist(v-bind:artist="artist")
 </template>
 
